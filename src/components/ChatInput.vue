851f5c28870d3592ff84ce595e6b2c717adb6d9a
kasradavatgaran <kasradavatgaran@yahoo.com>
2025-12-08 19:00:13 +0330

    feat: add ChatInput.vue component for voice recording and message input

---
+++ src/components/ChatInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="chat-input-area">
+    <div class="chat-box" :class="{ disabled: !isActive || isSubmitting }">
+      <button 
+        @mousedown="startRecording"
+        @mouseup="stopRecording"
+        @touchstart.prevent="startRecording"
+        @touchend.prevent="stopRecording"
+        class="mic-button" 
+        :disabled="!isActive || isSubmitting"
+      >
+        <span v-if="!isRecording">🎙️</span>
+        <span v-else class="recording-indicator">🛑</span>
+      </button>
+      
+      <input 
+        type="text" 
+        v-model="userMessage"
+        :placeholder="placeholder" 
+        :disabled="!isActive || isSubmitting"
+        @keyup.enter="sendMessage"
+      >
+      <span class="send-icon" @click="sendMessage">➤</span>
+    </div>
+  </div>
+</template>
